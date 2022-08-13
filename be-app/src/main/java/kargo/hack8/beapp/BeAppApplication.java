485fc1a16ede31f3977e3e2e5054b85aa3279ab4
adehndr <adehendra@outlook.com>
2022-08-13 09:46:07 +0700

    init spring

---
+++ be-app/src/main/java/kargo/hack8/beapp/BeAppApplication.java
@@ -0,0 +1,13 @@
+package kargo.hack8.beapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeAppApplication.class, args);
+	}
+
+}
---
+++ be-app/src/test/java/kargo/hack8/beapp/BeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kargo.hack8.beapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
